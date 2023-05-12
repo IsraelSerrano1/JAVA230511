9e9222d2468036f63a0ddf42c89c47f2f71179f3
IsraelSerrano1 <israel.serrano@softtek.com>
2023-05-12 12:49:11 +0200

    Renombrar clase main original para evitar confusion

--- src/com/softtek/presentacion/MainCampos.java
+++ src/com/softtek/presentacion/MainCampos.java
@@ -4,7 +4,7 @@ import com.softtek.modelo.Campos;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
-public class Main {
+public class MainCampos {
     public static void main(String[] args) {
 
         Campos num = new Campos(5);
